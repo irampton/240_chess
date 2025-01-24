52943605a47a8d89674565bab83e0e4b8ad7a389
irampton <irampton@users.noreply.github.com>
2025-01-24 16:24:09 -0700

    Removed logging

--- shared/src/test/java/passoff/chess/ChessBoardTests.java
+++ shared/src/test/java/passoff/chess/ChessBoardTests.java
@@ -33,30 +33,8 @@ public class ChessBoardTests {
     public void defaultGameBoard() {
         var expectedBoard = TestUtilities.defaultBoard();
 
-        // Log piece type and team at (0,0) and (8,8)
-        for (int i = 1; i <= 8; i++) {
-            for (int j = 1; j <= 8; j++) {
-                ChessPosition position00 = new ChessPosition(i, j);
-                ChessPiece piece00 = expectedBoard.getPiece(position00);
-                if (piece00 != null) {
-                    System.out.println("Piece at (" + i + "," + j + "): " + piece00.getPieceType() + " of team " + piece00.getTeamColor());
-                }
-            }
-        }
-
-        System.out.println("\nHEY!!!\n");
-
         var actualBoard = new ChessBoard();
         actualBoard.resetBoard();
-
-        for (int i = 1; i <= 8; i++) {
-            for (int j = 1; j <= 8; j++) {
-                ChessPiece piece00 = actualBoard.getPiece(new ChessPosition(i, j));
-                if (piece00 != null) {
-                    System.out.println("Piece at (" + i + "," + j + "): " + piece00.getPieceType() + " of team " + piece00.getTeamColor());
-                }
-            }
-        }
 
         Assertions.assertEquals(expectedBoard, actualBoard);
     }
