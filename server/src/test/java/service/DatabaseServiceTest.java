424c0db671a5a3b693fc598f87ca99bf2875af54
irampton <irampton@users.noreply.github.com>
2025-03-02 00:44:58 -0700

    Database service test

--- server/src/test/java/service/DatabaseServiceTest.java
+++ server/src/test/java/service/DatabaseServiceTest.java
@@ -1,4 +1,50 @@
 package service;
 
+import dataaccess.AuthDAO;
+import dataaccess.GameDAO;
+import dataaccess.UserDAO;
+import model.AuthData;
+import model.UserData;
+import org.junit.jupiter.api.*;
+
 public class DatabaseServiceTest {
+    private DatabaseService databaseService;
+
+    private AuthDAO authDAO;
+    private GameDAO gameDAO;
+    private UserDAO userDAO;
+
+    @BeforeEach
+    public void setUp() {
+        databaseService = new DatabaseService();
+
+        authDAO = new AuthDAO();
+        gameDAO = new GameDAO();
+        userDAO = new UserDAO();
+    }
+
+    // Positive Test Case: All DAO clear methods succeed
+    @Test
+    @Order(1)
+    @DisplayName("Clear returns without errors")
+    public void testClearAllSuccess() {
+        String authToken = "";
+        int gameId = 0;
+
+        try {
+            authToken = authDAO.createAuth("username").getAuthToken();
+            gameId = gameDAO.createGame("game1").getGameID();
+            userDAO.createUser(new UserData("username", "password", "email"));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        int result = databaseService.clearAll();
+
+        Assertions.assertEquals(1, result);
+        Assertions.assertNull(authDAO.getAuth(authToken));
+        Assertions.assertNull(gameDAO.getGame(gameId));
+        Assertions.assertNull(userDAO.getUser("username"));
+
+    }
 }
