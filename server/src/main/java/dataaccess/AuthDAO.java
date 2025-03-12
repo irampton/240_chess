68cc0b1797964aa08846eaf2e71d57771432ff6f
irampton <irampton@users.noreply.github.com>
2025-03-11 22:24:27 -0600

    AuthDAO to DB

--- server/src/main/java/dataaccess/AuthDAO.java
+++ server/src/main/java/dataaccess/AuthDAO.java
@@ -1,20 +1,23 @@
 package dataaccess;
 
 import model.AuthData;
+import org.mindrot.jbcrypt.BCrypt;
 
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 
 public class AuthDAO {
-    // Static list to store UserData in memory
-    private static List<AuthData> authDataList = new ArrayList<>();
-
     // Clears all games from the database
     // returns 1 if successful, error code if otherwise
     public int clear() {
         try {
-            authDataList.clear();
+            try (var conn = DatabaseManager.getConnection()) {
+                try (var preparedStatement = conn.prepareStatement("DELETE FROM auth_data")) {
+                    preparedStatement.executeUpdate();
+                }
+            }
             return 1; // Success
         } catch (Exception e) {
             return -1; // Error (Codes will be updated when adding the database)
@@ -22,25 +25,59 @@ public class AuthDAO {
     }
 
     // Create AuthData and add to the list
-    public AuthData createAuth(String username) {
+    public AuthData createAuth(String username) throws DataAccessException {
         AuthData authData = new AuthData(UUID.randomUUID().toString(), username);
-        authDataList.add(authData);
+
+        try (var conn = DatabaseManager.getConnection()) {
+            String query = "INSERT INTO auth_data (authToken, username) VALUES (?, ?)";
+            try (var preparedStatement = conn.prepareStatement(query)) {
+                preparedStatement.setString(1, authData.getAuthToken());
+                preparedStatement.setString(2, authData.getUsername());
+
+                preparedStatement.executeUpdate();
+            }
+        } catch (SQLException | DataAccessException e) {
+            e.printStackTrace();
+            throw new DataAccessException("Error creating user.");
+        }
 
         return authData;
     }
 
     // Get AuthData based on token
     public AuthData getAuth(String token) {
-        for (AuthData auth : authDataList) {
-            if (auth.getAuthToken().equals(token)) {
-                return auth;
+        String query = "SELECT authToken, username FROM auth_data WHERE authToken = ?";
+        try (var conn = DatabaseManager.getConnection()) {
+            try (var preparedStatement = conn.prepareStatement(query)) {
+                preparedStatement.setString(1, token);
+                try (var rs = preparedStatement.executeQuery()) {
+
+                    if (rs.next()) {
+                        return new AuthData(
+                                rs.getString("authToken"),
+                                rs.getString("username")
+                        );
+                    }
+                }
             }
+        } catch (SQLException | DataAccessException e) {
+            e.printStackTrace();
+            throw new RuntimeException(e);
         }
         return null;
     }
 
     // Delete AuthData from the list
     public void deleteAuth(AuthData authData) {
-        authDataList.remove(authData);
+        String query = "DELETE FROM auth_data WHERE authToken = ?";
+        try (var conn = DatabaseManager.getConnection()) {
+            try (var preparedStatement = conn.prepareStatement(query)) {
+                preparedStatement.setString(1, authData.getAuthToken());
+                preparedStatement.executeUpdate();
+            }
+        } catch (SQLException | DataAccessException e) {
+            e.printStackTrace();
+            throw new RuntimeException(e);
+        }
     }
 }
