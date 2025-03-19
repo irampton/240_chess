54a458008e8f8f60f8cec052f0a6537f14e538c4
irampton <irampton@users.noreply.github.com>
2025-03-19 17:18:35 -0600

    command state path

--- client/src/main/java/ui/GameState.java
+++ client/src/main/java/ui/GameState.java
@@ -17,20 +17,51 @@ public class GameState {
     }
 
     public void getCommand() {
-        System.out.print("[LOGGED_OUT] >>> ");
+        switch (currentState) {
+            case LOGGED_OUT:
+                System.out.print("[LOGGED_OUT] >>> ");
+                break;
+            case LOGGED_IN:
+                System.out.print("[LOGGED_IN] >>> ");
+                break;
+            case IN_GAME:
+                System.out.print("[IN_GAME] >>> ");
+                break;
+        }
         String line = scanner.nextLine();
         var command = line.split(" ");
-
-        switch (command[0]) {
-            case "help":
-                System.out.print("this is a list of commands: \n");
+        switch (currentState) {
+            case LOGGED_OUT:
+                switch (command[0].toLowerCase()) {
+                    case "help":
+                        System.out.print("this is a list of commands: \n");
+                        break;
+                    case "quit":
+                        System.out.println("Goodbye!");
+                        System.exit(0);
+                        break;
+                    case "register":
+                        break;
+                    case "login":
+                        break;
+                    default:
+                        System.out.println("Invalid command");
+                }
                 break;
-            case "quit":
-                System.out.println("Goodbye!");
-                System.exit(0);
+            case LOGGED_IN:
+                switch (command[0].toLowerCase()) {
+                    default:
+                        System.out.println("Invalid command");
+                }
                 break;
-            default:
-                System.out.println("Invalid command");
+            case IN_GAME:
+                switch (command[0].toLowerCase()) {
+                    case "leave":
+                        break;
+                    default:
+                        System.out.println("Invalid command");
+                }
+                break;
         }
     }
 }
