34168de7c6022d1ccbe70cb6862bf7e99ec271f6
irampton <irampton@users.noreply.github.com>
2025-04-10 17:49:30 -0600

    pawn promotion & resign

--- client/src/main/java/ui/GameState.java
+++ client/src/main/java/ui/GameState.java
@@ -1,9 +1,6 @@
 package ui;
 
-import chess.ChessBoard;
-import chess.ChessGame;
-import chess.ChessMove;
-import chess.ChessPosition;
+import chess.*;
 import model.*;
 
 import java.util.List;
@@ -27,6 +24,7 @@ public class GameState {
     private Boolean suppressNextOutput = false;
     private Boolean printInGame = false;
     private String username;
+    private boolean resignedOnce = false;
 
     public GameState() {
         currentState = State.LOGGED_OUT;
@@ -329,14 +327,17 @@ public class GameState {
         try {
             switch (command[0].toLowerCase()) {
                 case "help":
+                    resignedOnce = false;
                     printInGameHelp();
                     printInGame = true;
                     break;
                 case "redraw":
+                    resignedOnce = false;
                     serverFacade.redrawBoard();
                     printInGame = true;
                     break;
                 case "highlight":
+                    resignedOnce = false;
                     if (command.length != 2) {
                         throw new IllegalArgumentException("Invalid number of arguments. Expected 2 arguments.");
                     }
@@ -346,20 +347,46 @@ public class GameState {
                     printInGame = true;
                     break;
                 case "move":
-                    if (command.length != 3) {
+                    resignedOnce = false;
+                    if (command.length < 3 || command.length > 4) {
                         throw new IllegalArgumentException("Invalid number of arguments. Expected 3 arguments.");
                     }
                     ChessPosition moveStart = parseChessPosition(command[1]);
                     ChessPosition moveEnd = parseChessPosition(command[2]);
 
-                    serverFacade.makeMove(new ChessMove(moveStart, moveEnd, null), gameID);
+                    ChessPiece.PieceType promotion = null;
+                    if (command.length == 4) {
+                        switch (command[3].toLowerCase()) {
+                            case "q":
+                                promotion = ChessPiece.PieceType.QUEEN;
+                                break;
+                            case "r":
+                                promotion = ChessPiece.PieceType.ROOK;
+                                break;
+                            case "b":
+                                promotion = ChessPiece.PieceType.BISHOP;
+                                break;
+                            case "k":
+                                promotion = ChessPiece.PieceType.KING;
+                                break;
+                        }
+                    }
+
+                    serverFacade.makeMove(new ChessMove(moveStart, moveEnd, promotion), gameID);
                     break;
                 case "leave":
+                    resignedOnce = false;
                     serverFacade.leaveGame(gameID);
                     currentState = State.LOGGED_IN;
                     break;
                 case "resign":
-                    serverFacade.resignGame(gameID);
+                    if(!resignedOnce){
+                        System.out.println("Type resign again to resign the game");
+                        resignedOnce = true;
+                        printInGame = true;
+                    } else {
+                        serverFacade.resignGame(gameID);
+                    }
                     break;
                 default:
                     throw new IllegalArgumentException("Invalid command");
